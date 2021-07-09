4bb1a75dfc9f937d727fb04ba13c4482aff3d553
ozkalai <melihozkalayy@gmail.com>
2021-07-09 23:07:46 +0300

    PatternList rendered at Home

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import PatternList from "../src/components/pattern-list";
+
 export default function Home() {
-  return <div className="bg-red-500 w-full h-screen">asda</div>;
+  return (
+    <div className="bg-red-500 w-full h-screen">
+      <PatternList />
+    </div>
+  );
 }
