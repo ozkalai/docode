229f569d987753b882c92bf5249502781d0d1ea2
ozkalai <melihozkalayy@gmail.com>
2021-07-09 23:07:11 +0300

    reactQuery wrapped

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import "tailwindcss/tailwind.css";
+import { setAxiosBaseUrl } from "../src/utils/axios";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+setAxiosBaseUrl();
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
