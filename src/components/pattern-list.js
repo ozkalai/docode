4dfadf10d25ee25e6933376062eaf8dde5e2df45
ozkalai <melihozkalayy@gmail.com>
2021-07-10 13:06:02 +0300

    useInfiniteQuery implemented

--- src/components/pattern-list.js
+++ src/components/pattern-list.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Thumbnail from "./thumbnail";
 import { useInfiniteQuery } from "react-query";
-import axios from "axios";
 import { getItems } from "../services/get-items";
+import useIntersectionObserver from "../../hook/useIntersectionObserver";
 
 function PatternList() {
   const {
@@ -13,8 +13,22 @@ function PatternList() {
     fetchNextPage,
     hasNextPage,
     error,
-  } = useInfiniteQuery("items", async ({ pageParam = 0 }) => {
-    return await getItems(pageParam);
+  } = useInfiniteQuery(
+    "items",
+    async ({ pageParam = 0 }) => {
+      return await getItems(pageParam);
+    },
+    {
+      getNextPageParam: (lastPage, pages) => lastPage.config.url.slice(-1) + 1,
+    }
+  );
+
+  const loadMoreButtonRef = React.useRef();
+
+  useIntersectionObserver({
+    target: loadMoreButtonRef,
+    onIntersect: fetchNextPage,
+    enabled: hasNextPage,
   });
 
   const mergedPages = React.useMemo(() => {
@@ -31,11 +45,24 @@ function PatternList() {
     mergedPages: mergedPages,
   });
   return (
-    <div>
-      {mergedPages.map((item) => (
-        <Thumbnail item={item} key={item.designName} />
-      ))}
-    </div>
+    <>
+      <div>
+        {mergedPages.map((item) => (
+          <Thumbnail item={item} key={item.designName} />
+        ))}
+      </div>
+      <button
+        ref={loadMoreButtonRef}
+        onClick={() => fetchNextPage()}
+        disabled={!hasNextPage || isFetchingNextPage}
+      >
+        {isFetchingNextPage
+          ? "Loading more..."
+          : hasNextPage
+          ? "Load Newer"
+          : "Nothing more to load"}
+      </button>
+    </>
   );
 }
 
