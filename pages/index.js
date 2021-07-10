47414d044556441ea06bb4d809a61042a91311a7
ozkalai <melihozkalayy@gmail.com>
2021-07-10 14:29:59 +0300

    Navigtaion comp added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Navigation from "../src/components/navigation";
 import PatternList from "../src/components/pattern-list";
 
 export default function Home() {
   return (
     <div className="bg-red-500 w-full h-screen">
+      <Navigation />
       <PatternList />
     </div>
   );
