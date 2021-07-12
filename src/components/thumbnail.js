878c7811056605031426ee87b742d38f07b517f4
ozkalai <melihozkalayy@gmail.com>
2021-07-12 23:02:04 +0300

    fixed some css

--- src/components/thumbnail.js
+++ src/components/thumbnail.js
@@ -1,27 +1,34 @@
 import React from "react";
 
 function Thumbnail({ item }) {
+  console.log(item);
   const year = new Date().getFullYear();
   const yearOfUpload = item && item.importDate.slice(0, 4);
+  const importDate =
+    item && item.importDate.replaceAll("-", "/").replace("T", " ");
+  const lastEditDate =
+    item && item.lastEditDate.replaceAll("-", "/").replace("T", " ");
+
   return (
-    <div className="m-4 text-sm lg:w-1/3 xl:w-1/5 2xl:w-1/6 lg:test-lg xl:text-sm ">
-      <div className="w-full h-80">
+    <div className="m-4 text-sm lg:w-1/3 xl:w-1/5 2xl:w-1/6 lg:test-lg xl:text-sm border-2 border-gray-400  ">
+      <div className="w-full h-80 transform transition duration-700 ease-in-out hover:scale-105 delay-400 cursor-pointer">
         <img
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover p-1"
           src={`http://talent.docode.com.tr/thumbnail/${item.thumbnail}.jpg`}
         />
       </div>
-      <div className="flex flex-col p-2 rounded-b-lg bg-gray-300 font-semibold text-md">
-        <div className="w-full flex justify-start pl-2 font-bold">
+      <div className="flex flex-col p-2   font-semibold text-md font-mono border-t-2 border-gray-400">
+        <div className="w-full flex justify-start pl-2 font-bold my-1">
           {item.designName}
         </div>
-        <div className="flex justify-between">
-          <div className="flex justify-center items-center p-2">
-            {item.tags[0]}
-          </div>
-          <div className="flex justify-center items-center p-2">
-            {item.tags[1]}
-          </div>
+        <div className="flex flex-col px-2 font-light">
+          {item.tags.map((tag) => {
+            return <span className="my-1">{tag}</span>;
+          })}
+        </div>
+        <div className="px-2 font-extralight text-xs">
+          <div className="mt-2 py-0.5">{`U: ${importDate} `}</div>
+          <div className="py-0.5">{`E: ${lastEditDate}`}</div>
         </div>
       </div>
     </div>
